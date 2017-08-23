b23f52a5400aa497916e3c772bc473eb6324c5a9
Mauricio Rico <mrico005@gmail.com>
2017-08-23 09:47:43 -0500

    Added factorymethod folder with files and class diagrams for hierarchy and templates

--- Clase/FactoryMethod/templates.cpp
+++ Clase/FactoryMethod/templates.cpp
@@ -1,1 +1,24 @@
 // Mauricio Rico Zavala
+#include <iostream>
+using namespace std;
+// Clase padre
+class FactoryProduct{
+public:
+    template <class Prod>
+    Prod* factoryMethod(){
+        return new Prod;
+    }
+};
+// Clase hijo
+class ProductA : public FactoryProduct{
+public:
+    void operation(){
+        std::cout << "Estoy en el producto A" << endl;
+    }
+};
+
+int main(){
+    FactoryProduct* p = new FactoryProduct;
+    ProductA *a = p-> factoryMethod<ProductA>();
+    a->operation();
+}
